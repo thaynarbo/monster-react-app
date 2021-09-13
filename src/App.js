bd4550dd57c508cacdb323679ac3d4e4d2b1b2a9
thaynar96 <thaynarbo@gmail.com>
2021-09-13 15:08:54 -0300

    fetching api and creating a class component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component.jsx';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			monsters: [],
+		};
+	}
+
+	// componentDidMount renderiza assim que a página carrega
+
+	async componentDidMount() {
+		const response = await fetch(
+			'https://jsonplaceholder.typicode.com/users'
+		);
+		const data = await response.json();
+		this.setState({ monsters: data });
+	}
+
+	render() {
+		return (
+			<div>
+				<CardList monsters={this.state.monsters} />
+			</div>
+		);
+	}
 }
 
 export default App;
